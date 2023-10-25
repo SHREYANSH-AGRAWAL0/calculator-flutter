b3b43e1a2cc2e920e00afa2a66f3abe766099989
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-10-25 18:06:56 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Calculator")),
+        ),
+        body: Column(children: [Text("body space")]),
+      ),
+    );
+  }
+}
