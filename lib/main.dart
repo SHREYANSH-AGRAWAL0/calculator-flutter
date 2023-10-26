e2c7526792a8f9d070be300dc0673615cb4466c8
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-10-27 00:07:07 +0530

    fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @OverRide
   Widget build(BuildContext context) {
     return MaterialApp(home: calculator());
   }
@@ -16,18 +15,15 @@ class MyApp extends StatelessWidget {
 class calculator extends StatelessWidget {
   const calculator({super.key});
 
-  @OverRide
   Widget build(BuildContext context) {
-
     return Material(
       child: Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Calculator")),
         ),
         body: Column(children: [
-          Container(
-            Text('Calculation'), Text('Answer')    
-          ),
+          Text('Calculation'),
+          Text('Answer'),
           Container(
               // for the numpad area
               )
